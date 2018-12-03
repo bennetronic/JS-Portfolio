4781d9db1797d0bf076906a50eeffe2b4662307d
James Bennetts <jamesbennetts@gmail.com>
2018-12-03 11:52:17 +0000

    Update build-a-library.js
    update

---
+++ build-a-library.js
@@ -0,0 +1,117 @@
+class Media {
+
+  constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckedOut;
+  }
+
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating );
+    return ratingsSum / this.ratings.length;
+  }
+
+  addRating(value) {
+    this.ratings.push(value);
+  }
+};
+
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+class CD extends Media {
+  constructor(artist, title, songs) {
+    super(title);
+    this._artist;
+    this._songs = [];
+  }
+
+  get artist() {
+    return this._artist;
+  }
+
+  get songs() {
+    return this._songs;
+  }
+
+  shuffle(songs) {
+    return this._songs;
+  }
+};
+
+console.log(CD.shuffle);
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
+
+const nevermind = new CD ('Nirvana', 'Nevermind', ['Smells Like teen spirit', 'Lithium', 'Polly']);
+nevermind.toggleCheckOutStatus();
+console.log(nevermind.isCheckedOut);
+nevermind.addRating(5);
+nevermind.addRating(5);
+nevermind.addRating(5);
+console.log(nevermind.getAverageRating());
